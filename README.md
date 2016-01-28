255b989c99e355547f5956e2c971f97de46d03d6
Conor Fennell <fennell.conor@gmail.com>
2016-01-28 19:17:17 +0000

    guide for running camera telegram bot on pi

---
+++ README.md
@@ -0,0 +1,27 @@
+# pi-camera-bot
+
+Install node on your raspberry pi:
+
+For Raspberry Pi 2 Model B
+
+1. wget https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-armv7l.tar.gz
+2. tar -xvf node-v4.2.6-linux-armv7l.tar.gz
+3. cd node-v4.2.6-linux-armv7l
+4. sudo cp -R * /usr/local/
+5. node -v //sanity check
+
+For Raspberry Pi model A+ or B+
+
+1. wget https://nodejs.org/dist/v4.2.6/node-v4.2.6-linux-armv6l.tar.gz
+2. tar -xvf node-v4.2.6-linux-armv6l.tar.gz
+3. cd node-v4.2.6-linux-armv6l
+4. sudo cp -R * /usr/local/
+5. node -v //sanity check
+
+To run pi-camera-bot:
+
+1. clone down pi-camera-bot
+2. cd ./pi-camera-bot
+3. npm install // Run from the command line to install any node modules referenced in the package.json
+4. Put your own bot token in the bot.js file "var BOT_TOKEN = 'your-bot-token-provided-by-botfather';"
+5. node bot.js // starts the bot; send '/photo' message from Telegram to take a pic.
---
+++ bot.js
@@ -0,0 +1,35 @@
+var TelegramBot = require('node-telegram-bot-api');
+var exec = require('child_process').exec;
+var BOT_TOKEN = 'your-bot-token-provided-by-botfather';
+var bot = new TelegramBot(BOT_TOKEN, {polling: true});
+
+var photoQueue = [];
+
+bot.onText(/\/photo/, function (msg) {
+  console.log('photo');
+  var length = photoQueue.length;
+  if (length > 0) {
+     photoQueue.unshift(msg);
+     if (length === 1) {
+       bot.sendMessage(msg.chat.id, 'Please wait, there is 1 photo to be taken before you.')
+     } else {
+       bot.sendMessage(msg.chat.id, 'Please wait, there are ' + length + ' photos to be taken before you.')
+     }
+  } else {
+    photoQueue.push(msg);
+    takeAndSendPhoto(photoQueue[photoQueue.length - 1]);
+  }
+});
+
+function takeAndSendPhoto(msg) {
+  bot.sendChatAction(msg.chat.id, 'upload_photo').then(function() {
+    console.log('take photo');
+    exec('raspistill -o /home/pi/camera-bot/a.jpg -w 1920 -vf -hf -h 1080 -q 15', function photoTaken() {
+      bot.sendPhoto(msg.chat.id, '/home/pi/camera-bot/a.jpg', {caption: 'Sweet pic'}).then(function nextPhoto() {
+        photoQueue.pop();
+        if (photoQueue.length > 0) {
+          takeAndSendPhoto(photoQueue[photoQueue.length - 1]);
+        }
+     });
+  })});
+}
